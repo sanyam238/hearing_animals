3d491c169a27fb2c269de54acc5d13a9ce193fd9
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-09-17 11:47:03 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,22 @@ classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models
 function modelReady(){
     classifier.classify(gotResults);
 }
-    
+    dog=0;
+    cat=0;
+    cow=0;
+    lion=0;
+background=0;
+
 function gotResults(error,results){
     if (error){
         console.error(error);
     }else{
         console.log(results);
+
+        random_r = Math.floor(Math.random()*255)+1;
+        random_g = Math.floor(Math.random()*255)+1;
+        random_b = Math.floor(Math.random()*255)+1;
+        document.getElementById("heading_accuracy").style.color = "rgb("+random_r+","+random_g+","+random_b+")";
+        
 }
 }
